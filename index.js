2be233a76835f7ba16b135be5895fcebe32b594e
Ob55 <brian55mwangi@gmail.com>
2024-01-15 11:30:17 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const gameWidth = 400; 
+  const dodgerWidth = 40; 
+
+  if (left < gameWidth - dodgerWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
